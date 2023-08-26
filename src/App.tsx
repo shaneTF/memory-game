96a19efe8d2192fb26fd4ec99e953d736d57ca0b
Shane Franklin <shanefranklin148@gmail.com>
2023-08-26 11:22:18 -0400

    Reveal numbers on click and update styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,35 @@ function App() {
     [4, 3, 5, 0],
   ]);
 
+  const [revealedGrid, setRevealedGrid] = useState(
+    new Array(grid.length)
+      .fill("")
+      .map(() => new Array(grid[0].length).fill(false))
+  );
+
+  console.log("revealedGrid", revealedGrid);
+
+  function handleCardClicked(rowIndex: number, colIndex: number) {
+    //reveal the clicked card
+    const newRevealedGrid = [...revealedGrid];
+    newRevealedGrid[rowIndex][colIndex] = true;
+    setRevealedGrid(newRevealedGrid);
+    //If 1 card has already been clicked prior
+    //If they both match, mark them as answered
+    //If they don't match, hide them after 1 second
+  }
   return (
     <div className="App">
       <div className="grid">
         {grid.map((row, rowIndex) => (
           <div key={rowIndex} className="row">
             {row.map((number, colIndex) => (
-              <div key={colIndex} className="card">
-                {number}
+              <div
+                onClick={() => handleCardClicked(rowIndex, colIndex)}
+                key={colIndex}
+                className="card"
+              >
+                {revealedGrid[rowIndex][colIndex] ? number : ""}
               </div>
             ))}
           </div>
