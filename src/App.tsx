c857865af936c8f79c8c90092492e8b503109ba5
Shane Franklin <shanefranklin148@gmail.com>
2023-08-26 12:14:48 -0400

    Added game win condition and updated card styling. Edge cases/additions can be addressed another time

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+/**
+ * Potential Additons
+ *  Changeable grid size UI
+ *  Fix ability to freely click on as many cards as you want before they are hidden again.
+ */
+
 import { useState } from "react";
 import "./App.css";
+
+type TCell = {
+  row: number;
+  col: number;
+};
 
 function App() {
   const [grid, setGrid] = useState([
@@ -16,14 +27,45 @@ function App() {
 
   console.log("revealedGrid", revealedGrid);
 
+  const [previousClick, setPreviousClick] = useState<TCell | undefined>();
+
   function handleCardClicked(rowIndex: number, colIndex: number) {
     //reveal the clicked card
+    if (revealedGrid[rowIndex][colIndex]) return;
+    const clickedNumber = grid[rowIndex][colIndex];
     const newRevealedGrid = [...revealedGrid];
     newRevealedGrid[rowIndex][colIndex] = true;
     setRevealedGrid(newRevealedGrid);
-    //If 1 card has already been clicked prior
-    //If they both match, mark them as answered
-    //If they don't match, hide them after 1 second
+
+    if (previousClick) {
+      const previousClickNumber = grid[previousClick.row][previousClick.col];
+      // second click of the 2 clicks
+
+      //If they don't match, hide them after 1 second
+      if (previousClickNumber !== clickedNumber) {
+        setTimeout(() => {
+          newRevealedGrid[rowIndex][colIndex] = false;
+          newRevealedGrid[previousClick.row][previousClick.col] = false;
+          setRevealedGrid([...newRevealedGrid]);
+        }, 1000);
+      } else {
+        // check if everything has been revealed, then show an alert
+        const hasWon = revealedGrid.flat().every((isRevealed) => isRevealed);
+        if (hasWon) {
+          setTimeout(() => {
+            alert("You Won!");
+          });
+        }
+      }
+
+      setPreviousClick(undefined);
+    } else {
+      // first click of the 2 clicks
+      setPreviousClick({
+        row: rowIndex,
+        col: colIndex,
+      });
+    }
   }
   return (
     <div className="App">
@@ -34,7 +76,9 @@ function App() {
               <div
                 onClick={() => handleCardClicked(rowIndex, colIndex)}
                 key={colIndex}
-                className="card"
+                className={
+                  "card " + (revealedGrid[rowIndex][colIndex] ? "revealed" : "")
+                }
               >
                 {revealedGrid[rowIndex][colIndex] ? number : ""}
               </div>
